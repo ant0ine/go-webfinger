d7f8887d1674ed385069732593711577988f97e9
antoine <antoine.imbert@gmail.com>
2012-11-25 04:00:02 +0000

    basic JRD parser

---
+++ jrd.go
@@ -0,0 +1,40 @@
+package webfinger
+
+import (
+	"encoding/json"
+)
+
+type JRD struct {
+	Subject    string
+	Expires    string
+	Aliases    []string
+	Links      []Link
+	Properties map[string]interface{}
+}
+
+type Link struct {
+	Rel        string
+	Type       string
+	Href       string
+	Titles     map[string]string
+	Properties map[string]interface{}
+	Template   string
+}
+
+func ParseJRD(blob []byte) (*JRD, error) {
+	jrd := JRD{}
+	err := json.Unmarshal(blob, &jrd)
+	if err != nil {
+		return nil, err
+	}
+	return &jrd, nil
+}
+
+func (self *JRD) GetLinkByRel(rel string) *Link {
+	for _, link := range self.Links {
+		if link.Rel == rel {
+			return &link
+		}
+	}
+	return nil
+}
---
+++ jrd_test.go
@@ -0,0 +1,64 @@
+package webfinger
+
+import (
+	"testing"
+)
+
+func TestParseJRD(t *testing.T) {
+
+        // from spec http://tools.ietf.org/html/rfc6415#appendix-A
+        blob := `
+        {
+              "subject":"http://blog.example.com/article/id/314",
+              "expires":"2010-01-30T09:30:00Z",
+
+              "aliases":[
+                "http://blog.example.com/cool_new_thing",
+                "http://blog.example.com/steve/article/7"],
+
+              "properties":{
+                "http://blgx.example.net/ns/version":"1.3",
+                "http://blgx.example.net/ns/ext":null
+              },
+
+              "links":[
+                {
+                  "rel":"author",
+                  "type":"text/html",
+                  "href":"http://blog.example.com/author/steve",
+                  "titles":{
+                    "default":"About the Author",
+                    "en-us":"Author Information"
+                  },
+                  "properties":{
+                    "http://example.com/role":"editor"
+                  }
+                },
+                {
+                  "rel":"author",
+                  "href":"http://example.com/author/john",
+                  "titles":{
+                    "default":"The other author"
+                  }
+                },
+                {
+                  "rel":"copyright",
+                  "template":"http://example.com/copyright?id={uri}"
+                }
+              ]
+            }
+        `
+        jrd, err := ParseJRD([]byte(blob))
+        if err != nil {
+		t.Fatal(err)
+        }
+        if jrd.Subject != "http://blog.example.com/article/id/314" {
+		t.Error()
+        }
+        if jrd.GetLinkByRel("copyright") == nil {
+		t.Error()
+        }
+        if jrd.GetLinkByRel("copyright").Template != "http://example.com/copyright?id={uri}" {
+		t.Error()
+        }
+}
