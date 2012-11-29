c5fa586b970fef8d3464897684117e9c930259d4
antoine <antoine.imbert@gmail.com>
2012-11-29 03:52:53 +0000

    doc indent

--- client.go
+++ client.go
@@ -10,34 +10,34 @@
 //
 // Example:
 //
-//         package main
+//  package main
 //
-//         import (
-//         	"fmt"
-//              "github.com/ant0ine/go-webfinger"
-//	        "os"
-//         )
+//  import (
+//          "fmt"
+//          "github.com/ant0ine/go-webfinger"
+//          "os"
+//  )
 //
-//         func main() {
-//	        email := os.Args[1]
+//  func main() {
+//          email := os.Args[1]
 //
-//	        client := webfinger.Client{
-//	        	EnableLegacyAPISupport: true,
-//	        }
+//          client := webfinger.Client{
+//                  EnableLegacyAPISupport: true,
+//          }
 //
-//	        resource, err := webfinger.MakeResource(email)
-//	        if err != nil {
-//		        panic(err)
-//	        }
+//          resource, err := webfinger.MakeResource(email)
+//          if err != nil {
+//                  panic(err)
+//          }
 //
-//	        jrd, err := client.GetJRD(resource)
-//	        if err != nil {
-//		        fmt.Println(err)
-//		        return
-//	        }
+//          jrd, err := client.GetJRD(resource)
+//          if err != nil {
+//                  fmt.Println(err)
+//                  return
+//          }
 //
-//	        fmt.Printf("JRD: %+v", jrd)
-//         }
+//          fmt.Printf("JRD: %+v", jrd)
+//  }
 package webfinger
 
 import (
