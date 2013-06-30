8e5efbf483aca463e9cc75319538d5fa34605efc
antoine <antoine.imbert@gmail.com>
2013-06-30 04:27:47 +0000

    New command line client
    * command line arguments parsing with "flag"
    * improved logging
    * Makefile
    
    $ ./webfinger
    webfinger [-vh] <resource uri>
      -h=false: display this message
      -v=false: print details about the resolution
    example: webfinger -v bob@example.com

---
+++ examples/cmdline/main.go
@@ -0,0 +1,57 @@
+// TODO
+// * improve JRD output
+// * do stuff with the JRD
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ant0ine/go-webfinger"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func printHelp() {
+	fmt.Println("webfinger [-vh] <resource uri>")
+	flag.PrintDefaults()
+	fmt.Println("example: webfinger -v bob@example.com") // same Bob as in the draft
+}
+
+func main() {
+
+	// cmd line flags
+	verbose := flag.Bool("v", false, "print details about the resolution")
+	help := flag.Bool("h", false, "display this message")
+	flag.Parse()
+
+	if *help {
+		printHelp()
+		os.Exit(0)
+	}
+
+	if !*verbose {
+		log.SetOutput(ioutil.Discard)
+	}
+
+	email := flag.Arg(0)
+
+	if email == "" {
+		printHelp()
+		os.Exit(1)
+	}
+
+	log.SetFlags(0)
+
+	client := webfinger.NewClient(nil)
+	client.AllowHTTP = true
+
+	jrd, err := client.Lookup(email, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("JRD: %+v", jrd)
+	os.Exit(0)
+}
